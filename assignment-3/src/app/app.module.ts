0fa94dce35127e66d0f1d0737737340cf9585ad8
Nikorasu00 <141564145+Nikorasu00@users.noreply.github.com>
2023-10-13 14:44:13 +1100

    Added components for student1

--- assignment-3/src/app/app.module.ts
+++ assignment-3/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { ListCategoryComponent } from './list-category/list-category.component';
+import { DeleteCategoryComponent } from './delete-category/delete-category.component';
+import { UpdateCategoryComponent } from './update-category/update-category.component';
+import { DisplayCategoryComponent } from './display-category/display-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategoryComponent,
+    ListCategoryComponent,
+    DeleteCategoryComponent,
+    UpdateCategoryComponent,
+    DisplayCategoryComponent
   ],
   imports: [
     BrowserModule,

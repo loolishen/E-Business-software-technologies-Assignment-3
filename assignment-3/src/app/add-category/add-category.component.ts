f88c6e7063ebbff9e4468752efbc5e44272d7442
Nikorasu00 <141564145+Nikorasu00@users.noreply.github.com>
2023-10-13 15:26:56 +1100

    Task1.1

--- assignment-3/src/app/add-category/add-category.component.ts
+++ assignment-3/src/app/add-category/add-category.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+// event-form.component.ts
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-add-category',
+  selector: 'add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
-
+export class AddCategoryComponent{
+  eventName: string = '';
+  description: string = '';
+  image: string = '';
+  submitForm(): void {
+    // Handle form submission logic here
+    console.log('Form submitted:', this.eventName, this.description, this.image);
+    // You can implement your HTTP request to send form data to the server
+  }
 }
